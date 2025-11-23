3e75d917c58417fb3349012da161e491263d802b
ShreedharG <shreedhar.goyal@gmail.com>
2025-11-23 16:18:32 +0530

    Basic backend -> frontend connection

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-require('dotenv').config()
+import express from 'express';
+import dotenv from 'dotenv';
 
-const express = require('express')
+const app = express();
+dotenv.config();
 
-const app = express()
 const port =  process.env.PORT || 4000; 
+
+app.get('/api/jokes', (req,res) => {
+    const jokes = [
+        {
+            id : 1,
+            title : 'joke1',
+            content : 'First Joke'
+        },
+        {
+            id : 2,
+            title : 'joke2',
+            content : 'First Joke'
+        },
+        {
+            id : 3,
+            title : 'joke3',
+            content : 'First Joke'
+        },
+        {
+            id : 4,
+            title : 'joke4',
+            content : 'First Joke'
+        },
+        {
+            id : 5,
+            title : 'joke5',
+            content : 'First Joke'
+        }
+    ];
+    res.send(jokes);
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,5 +51,5 @@ app.get('/login', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Serve at http://localhost:${port}`)
 })
