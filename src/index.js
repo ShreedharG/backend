0f397d4a50ea3cc8b526a33272a4b06d740c1516
ShreedharG <shreedhar.goyal@gmail.com>
2025-12-17 02:19:52 +0530

    Added app.js for express lib; Utils codes for api call error and responses; configurations of cors; and express json, url and public assets

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
+import {app} from "./app.js";
 dotenv.config({
   path: "./.env",
 });
 
 import connectDb from "./db/index.js";
 
-;(connectDb)()
+(connectDb)()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+  })
+})
+.catch((error) => {
+  console.log("Error in DB connection!!!", error);
+})
 
 
 
